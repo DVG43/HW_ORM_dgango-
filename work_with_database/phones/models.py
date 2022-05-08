49aaa60184cf0a37f80b5a1d0c4666ee92eada19
Galuta Dmitrii <dvgaluta@gmail.com>
2022-05-09 00:57:37 +0300

    commit_3_start

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
+    name = models.CharField(max_length=150)
+    prise = models.FloatField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slag = models.SlugField(max_length=150, db_column=name)
+
     pass
+
+
+
+#`name`, `price`, `image`, `release_date`, `lte_exists` и `slug`
+
+
